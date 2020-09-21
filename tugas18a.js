8b0d6f69410e2f4e7fca0f5aa86c7d65ff8132ea
hasanhabsyi2011 <71365610+hasanhabsyi2011@users.noreply.github.com>
2020-09-21 11:46:05 +0700

    Add files via upload

---
+++ tugas18a.js
@@ -0,0 +1,4 @@
+var kalimat ="It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout."
+var splitKalimat = kalimat.split(" ")
+
+console.log(splitKalimat[3],"",splitKalimat[4])
